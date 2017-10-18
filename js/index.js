7ab9db21aaecb823d76834905ba37469985cf62e
Christian Wang <christian.gnaw@gmail.com>
2017-10-17 23:13:19 -0400

    Added engine code and some helper classes

---
+++ js/index.js
@@ -0,0 +1,57 @@
+let a = new Actor();
+a.bindToElement(document.getElementById("actor"));
+a.styleElement({
+  "height": '100px',
+  "width": '100px',
+  "backgroundColor": 'blue',
+  "position": 'absolute',
+  "top": '0px',
+  "left": '0px'
+});
+
+a.init = function() {
+  a.vx = 1;
+  a.vy = 1;
+  a.x = 100;
+  a.y = 100;
+}
+
+a.init();
+
+a.update = function() {
+  a.setLocation(a.x + a.vx, a.y + a.vy);
+
+  if (a.getLocation().x > window.innerWidth - 100) {
+    a.vx *= -1;
+  } else if (a.getLocation().x < 0) {
+    a.vx *= -1
+  };
+
+  if (a.getLocation().y > window.innerHeight - 100) {
+    a.vy *= -1;
+  } else if (a.getLocation().y < 0) {
+    a.vy *= -1
+  }
+}
+
+let stage = new Stage();
+stage.bindToElement(document.getElementById("stage"));
+
+stage.addObject(a);
+
+stage.update = function() {
+  //  if (stage.objects)
+  stage.objects.forEach(function(obj) {
+    obj.update();
+  });
+  console.log(stage.refreshUpdate.currentRPS);
+}
+
+stage.render = function() {
+  if (stage.objects)
+    stage.objects.forEach(function(obj) {
+      obj.update();
+    });
+}
+
+stage.start(120, 60);
---
+++ js/refresh.js
@@ -0,0 +1,32 @@
+/**
+ * Controls the rate at which a function is looped
+ **/
+class Refresh {
+  //Function that is to be run at RPS(refreshes per seconds)
+  constructor(funct, rps) {
+    this.funct = funct;
+    this.rps = rps;
+    this.start(this.rps);
+  }
+
+  start() {
+    this.stop = false;
+    this.rpsInterval = 1000 / this.rps;
+    this.timer = new Timer();
+    this.loop();
+  }
+
+  loop() {
+    if (this.stop) return;
+    //Request a frame refresh
+    window.requestAnimationFrame(this.loop.bind(this));
+
+    //If enough time has passed call the function
+    let millisElapsed = this.timer.millisecondsElapsed();
+    this.currentRPS = millisElapsed / this.rpsInterval * this.rps;
+    if (millisElapsed > this.rpsInterval) {
+      this.timer.mark();
+      this.funct();
+    }
+  }
+}
