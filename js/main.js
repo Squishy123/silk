938b753776c9b6c48424acb673ae8aab0b7ffafb
Christian Wang <christian.gnaw@gmail.com>
2017-10-19 23:02:45 -0400

    Added quadtree implementation for spatial object management

--- js/main.js
+++ js/main.js
@@ -6,11 +6,7 @@ s.styleElement({
   "background-color": "#000000"
 });
 
-let p1 = new Paddle();
+let p1 = new Player();
 s.addObject(p1);
-p1.start(100,100);
 
-let p2 = new Paddle();
-s.addObject(p2);
-p2.start(100,100);
-p2.setLocation({x: p2.element.parentElement.clientWidth-p2.getDimensions().width, y: p2.y});
+console.log(s.getObjects());
