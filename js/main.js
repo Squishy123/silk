7380d34d51ab91d3a2b81756e78b829c3c84ec50
Christian Wang <christian.gnaw@gmail.com>
2017-10-26 21:34:55 -0400

    Removed the buttons to start and stop

--- js/main.js
+++ js/main.js
@@ -3,24 +3,19 @@ s.styleElement({
   "background-color": "#000000"
 });
 
-
 s.setBounds({
   width: "75vh",
   height: "75vh",
   x: "50%",
   y: "50%"
 });
+
+s.start();
+
 let tiles = [new Tile(), new Tile(), new Tile()];
+tiles.forEach(function(t) {
+  s.addObject(t);
+});
+
 let p1 = new Player();
-
-function start() {
-  s.start();
-  tiles.forEach(function(t) {
-    s.addObject(t);
-  });
-  s.addObject(p1);
-}
-
-function stop() {
-  s.stop();
-}
+s.addObject(p1);
