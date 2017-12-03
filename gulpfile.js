d3f1259ad0675cc025639ab2e97da82eb7bdbb42
Christian Wang <christian.gnaw@gmail.com>
2017-12-02 23:11:39 -0500

    Fixed some paths and added cleanup task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 // Include gulp
 const gulp = require('gulp');
 
-// Include plugins
+// Include gulp plugins
 const [jshint, concat, rename, uglify, util] = [require('gulp-jshint'), require('gulp-concat'), require('gulp-rename'), require('gulp-uglify'), require('gulp-util')];
 
-const [SRC_PATH, APP_PATH, BUILD_PATH] = ['src/lib/*.js', 'app', 'build'];
+// Include node plugins
+const [del] = [require('del')];
+
+const [SRC_PATH, APP_PATH, BUILD_PATH] = ['src', 'app', 'build'];
 gulp.task('default', function() {
   util.log("== Welcome to Silk-Generator ==")
 });
@@ -21,7 +24,7 @@ gulp.task('build-library', function() {
       esversion: 6
     }))
     .pipe(jshint.reporter('default'))
-    .pipe(gulp.dest(`${APP_PATH}/src/js/lib`))
+    .pipe(gulp.dest(`${APP_PATH}/src/js/lib/`))
     .on('end', () => {
       util.log("== Finished Building Library ==")
     });
@@ -29,15 +32,27 @@ gulp.task('build-library', function() {
 
 gulp.task('build-webpage', function() {
   gulp.src(`${SRC_PATH}/index.html`)
-    .pipe(gulp.dest(`${SRC_PATH}`))
+    .pipe(gulp.dest(`${APP_PATH}`))
 });
 
 gulp.task('export', function() {
   util.log("== Starting Export ==")
-  gulp.src(`${APP_PATH}/src/lib/*.js`)
+  gulp.src(`${APP_PATH}/src/js/lib/*.js`)
     .pipe(concat('silk-lib.js'))
     .pipe(gulp.dest(`${BUILD_PATH}/src/lib`))
     .on('end', () => {
       util.log("== Finished Export ==")
     });
+});
+
+gulp.task('cleanup', function() {
+  util.log("== Cleaning up files ==")
+  //delete app files
+  del(APP_PATH, {
+    force: true
+  });
+  //delete build
+  del(BUILD_PATH, {
+    force: true
+  });
 });
