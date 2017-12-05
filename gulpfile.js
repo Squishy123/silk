63bbf88dd8d6c3da6764ce4366f058aed49b8144
Christian Wang <christian.gnaw@gmail.com>
2017-12-05 01:32:19 -0500

    Added SRCwatcher task

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,44 @@
 const gulp = require('gulp');
 
 // Include gulp plugins
-const [jshint, concat, rename, uglify, util] = [require('gulp-jshint'), require('gulp-concat'), require('gulp-rename'), require('gulp-uglify'), require('gulp-util')];
+const [jshint, concat, rename, uglify, util, connect] = [require('gulp-jshint'), require('gulp-concat'), require('gulp-rename'), require('gulp-uglify'), require('gulp-util'), require('gulp-connect')];
 
 // Include node plugins
-const [del,chalk] = [require('del'), require('chalk')];
+const [del, chalk] = [require('del'), require('chalk')];
 
 const [SRC_PATH, APP_PATH, BUILD_PATH] = ['src', 'app', 'build'];
 gulp.task('default', function() {
   util.log(chalk.bgCyan(chalk.black("== Welcome to Silk-Generator ==")))
 });
 
-gulp.task('start-app', ['default', 'build-library'], function() {
+//start the server
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+//update html
+gulp.task('updateHTML', function() {
+  gulp.src(`${SRC_PATH}/*.html`)
+    .pipe(gulp.dest(`${APP_PATH}/`, {ext: '.html'}));
+  gulp.src(`${APP_PATH}/*.html`)
+    .pipe(connect.reload());
+});
+
+//watch for changes
+gulp.task('watchSRC', function() {
+  gulp.watch([`${SRC_PATH}/*.html`], ['updateHTML'])
+});
+
+gulp.task('start-app', ['default', 'build-library', 'build-webpage'], function() {
   util.log(chalk.bgCyan(chalk.black("== Starting App ==")))
 
 });
 
 gulp.task('build-library', function() {
-util.log(chalk.bgCyan(chalk.black("== Building Library ==")))
+  util.log(chalk.bgCyan(chalk.black("== Building Library ==")))
   return gulp.src(`${SRC_PATH}/js/lib/*.js`)
     .pipe(jshint({
       esversion: 6
@@ -26,7 +47,7 @@ util.log(chalk.bgCyan(chalk.black("== Building Library ==")))
     .pipe(jshint.reporter('default'))
     .pipe(gulp.dest(`${APP_PATH}/src/js/lib/`))
     .on('end', () => {
-util.log(chalk.bgCyan(chalk.black("== Finished Build ==")))
+      util.log(chalk.bgCyan(chalk.black("== Finished Build ==")))
     });
 });
 
@@ -36,17 +57,17 @@ gulp.task('build-webpage', function() {
 });
 
 gulp.task('export', function() {
-util.log(chalk.bgCyan(chalk.black("== Starting Export ==")))
+  util.log(chalk.bgCyan(chalk.black("== Starting Export ==")))
   gulp.src(`${APP_PATH}/src/js/lib/*.js`)
     .pipe(concat('silk-lib.js'))
     .pipe(gulp.dest(`${BUILD_PATH}/src/lib`))
     .on('end', () => {
-util.log(chalk.bgCyan(chalk.black("== Finished Export ==")))
+      util.log(chalk.bgCyan(chalk.black("== Finished Export ==")))
     });
 });
 
 gulp.task('cleanup', function() {
-util.log(chalk.bgCyan(chalk.black("== Cleaning Up ==")))
+  util.log(chalk.bgCyan(chalk.black("== Cleaning Up ==")))
   //delete app files
   del(APP_PATH, {
     force: true
